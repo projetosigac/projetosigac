6aadde8e5940df407f36d6f4cde2c398bd26957b
Paulo Henrique Aguiar <phaguiardm@gmail.com>
2015-09-23 20:01:09 -0300

    Fix directory structure

--- BombeirosRescaldo/Simulation/BombeirosRescaldo_newmapping.h
+++
@@ -1,6 +0,0 @@
-/* BombeirosRescaldo_newmapping.h */
-
-#include "Main.h"
-
-extern outC_Main outputs_ctx;
-
--- BombeirosRescaldo/Simulation/Main_dll.c
+++
@@ -1,4 +0,0 @@
-const char*  pszDllPathname = "Main.dll";
-
-const char*  pszLauncherPathname = "C:/Program Files (x86)/Esterel Technologies/Esterel SCADE 6.4.2/SCADE/bin/SCSSMLNC.exe";
-
