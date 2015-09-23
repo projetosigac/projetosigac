61a80a2a497126ad6a70381d8ce6111dee27228a
Danilo Ramalho <danilovex@gmail.com>
2015-09-23 01:50:54 -0300

    indicao de lat e long, clicar no enter localiza local, traça rota para base da samu

--- web/public/js/mapa.js
+++ web/public/js/mapa.js
@@ -12,11 +12,14 @@ mapa = function(){
 				}
 			}
 		});
-	});
-		
+	});		
 	}
+    
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+        
 	var _initialize = function (){
-			
+        
 		var latlng = new google.maps.LatLng(-23.19586486253487, -45.89185186132812);
 		var options = {
 			zoom: 10,
@@ -24,6 +27,9 @@ mapa = function(){
 			mapTypeId: google.maps.MapTypeId.ROADMAP
 		};
 		map = new google.maps.Map(document.getElementById("map"), options);
+        
+        directionsDisplay.setMap(map);
+        
 		geocoder = new google.maps.Geocoder();
 		marker = new google.maps.Marker({
 			map: map,
@@ -32,36 +38,55 @@ mapa = function(){
 		});
 		marker.setPosition(latlng);
 				
-	}
+	}    
+    var _calcularRota = function (enderecoAtendimento) {
+      directionsService.route({
+        origin: enderecoAtendimento,
+        destination: "Avenida Andrômeda, 1000 Jardim Satélite São José dos Campos",
+        travelMode: google.maps.TravelMode.DRIVING
+      }, function(response, status) {
+        if (status === google.maps.DirectionsStatus.OK) {
+          directionsDisplay.setDirections(response);
+        } else {
+          window.alert('Directions request failed due to ' + status);
+        }
+      });
+    }
 	var _carregarNoMapa = function (endereco) {
 		geocoder.geocode({ 'address': endereco + ', Brasil', 'region': 'BR' }, function (results, status) {
 			if (status == google.maps.GeocoderStatus.OK) {
 				if (results[0]) {
 					var latitude = results[0].geometry.location.lat();
 					var longitude = results[0].geometry.location.lng();
+                    var enderecoFormatado = results[0].formatted_address;
+                    
+                    $("#enderecoAtendimentoGoogle").val(enderecoFormatado);
 		
-					//$('#txtEndereco').val(results[0].formatted_address);
-					//$('#txtLatitude').val(latitude);
-                   	//$('#txtLongitude').val(longitude);
+                    $("#mapaLegenda").html("Posição geográfica da emergência, latitude: ".concat(latitude," e longitude: ",longitude,"."));
 		
 					var location = new google.maps.LatLng(latitude, longitude);
 					marker.setPosition(location);
 					marker.setVisible(true);
 					map.setCenter(location);
 					map.setZoom(16);
+                    
+                    var infowindow = new google.maps.InfoWindow({
+                        content: "<b>Local da emergência</b><br/>".concat(enderecoFormatado),
+                        maxWidth: 250
+                    });
+                    
+                    marker.addListener('click', function() {
+                        infowindow.open(map, marker);
+                    });
 					
 				}
 			}
-		})
-	}
-	var _pesquisarEndereco = function(){
-		_carregarNoMapa($("#localEmergencia").val());
-		return false;
+		});
 	}
 	return {
 		start: _start,
 		initialize: _initialize,
 		carregarNoMapa: _carregarNoMapa,
-		pesquisarEndereco: _pesquisarEndereco
+        calcularRota:_calcularRota
 	}
 }();
