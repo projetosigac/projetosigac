47b1a115371f820066f1dcf319e401c8cc3fe85a
rshigemura <rafael.shigemura@gmail.com>
2015-10-07 23:29:23 -0300

    Nova API para utilização de BD Sqlite

---
+++ SIGAC_DB_Sqlite/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sqlite3.h"
+  
+  static int callback(void *NotUsed, int argc, char **argv, char **azColName){
+    int i;
+    for(i=0; i<argc; i++){
+      printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+    }
+    printf("\n");
+    return 0;
+  }
+  
+  int main(int argc, char **argv){
+    sqlite3 *db;
+    char *zErrMsg = 0;
+    int rc;
+  
+    if( argc!=3 ){
+      fprintf(stderr, "Uso: %s Nome_DB ComandoSQL\n", argv[0]);
+      return(1);
+    }
+    rc = sqlite3_open(argv[1], &db);
+    if( rc ){
+      fprintf(stderr, "Não foi possível abrir o DB: %s\n", sqlite3_errmsg(db));
+      sqlite3_close(db);
+      return(1);
+    }
+    rc = sqlite3_exec(db, argv[2], callback, 0, &zErrMsg);
+    if( rc!=SQLITE_OK ){
+      fprintf(stderr, "Erro de SQL: %s\n", zErrMsg);
+      sqlite3_free(zErrMsg);
+    }
+    sqlite3_close(db);
+    return 0;
+  }
