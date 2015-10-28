1a9b7d9e85c7be4801ce6393c72cbdb9ad24dcfe
Joao Jardim Neto <jrdmnt@gmail.com>
2015-10-28 17:22:53 -0200

    Bug fixes em cadastro de equipamentos e criando botões de deleção de equipamentos.

--- web/public/js/pages/cadastroEquipamento.js
+++ web/public/js/pages/cadastroEquipamento.js
@@ -2,7 +2,6 @@ cadastroEquipamento = function(){
 	var _api_insert_equip = function (){
 
 		var requestData = JSON.stringify($('#formInsertEquip').serializeObject());
-		//alert(requestData);
 		$.ajax({
 			url: '/api/insert-equip',
 			type: 'POST',
@@ -11,19 +10,37 @@ cadastroEquipamento = function(){
 			dataType: 'json',
 			data: requestData
 		}).done(function(data, textStatus, jqXHR) {
-			//window.location.href = '/dashboard';
 			alert("Equipamento Cadastrado");
 		}).fail(function(jqXHR, textStatus, errorThrown) {
 			alert(jqXHR.responseJSON.message);
 		});
 
-		
+		return false;
+	}
 
-		
+	var _api_delete_equip = function (){
+
+		var requestData = JSON.stringify($('#formInsertEquip').serializeObject());
+		//alert(requestData);
+		$.ajax({
+			url: '/api/delete-equip',
+			type: 'POST',
+			async: false,
+			contentType: 'application/json; charset=utf-8',
+			dataType: 'json',
+			data: requestData
+		}).done(function(data, textStatus, jqXHR) {
+			alert("Equipamento Deletado");
+		}).fail(function(jqXHR, textStatus, errorThrown) {
+			alert(jqXHR.responseJSON.message);
+		});
 
 		return false;
 	}
+
+
 	return {
-		api_insert_equip: _api_insert_equip
+		api_insert_equip: _api_insert_equip,
+		api_delete_equip: _api_delete_equip
 	}
 }();
