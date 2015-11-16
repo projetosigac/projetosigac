230149a77ed9f925ba85c09b7a0bb8aab98ca0bc
Marcelo Almeida <marcelocralmeida@gmail.com>
2015-11-16 01:03:59 -0200

    Send information once and provide feedback using led

--- sigac-rfid-reader/sigac-rfid-reader.py
+++ sigac-rfid-reader/sigac-rfid-reader.py
@@ -5,13 +5,25 @@ import MFRC522
 import requests
 import json
 import signal
+import subprocess
+import sys
+import time
 
+
+if len(sys.argv) != 2:
+    DISPATCHER = 'console'
+else:
+    DISPATCHER = sys.argv[1]
+    if DISPATCHER not in ['web', 'console']:
+        print 'Action "%s" not supported yet.' % DISPATCHER
+        exit()
 
 CONTINUE_READING = True
+RFID_TAGS = set()
 
 
-# Capture SIGINT for cleanup when the script is aborted.
-def sigint_callback(signal, frame):
+# Capture SIGINT for cleanup when the script is aborted
+def end_read(signal,frame):
     global CONTINUE_READING
     print
     print "Ctrl + C captured. The script will terminate."
@@ -20,11 +32,19 @@ def sigint_callback(signal, frame):
 
 
 def initialize():
-    # Hook the SIGINT.
-    signal.signal(signal.SIGINT, sigint_callback)
+    # Hook the SIGINT
+    signal.signal(signal.SIGINT, end_read)
 
-    # Welcome message.
     print "SIGAC RFID reading module. "
+    print
+
+    modprobe = subprocess.call(['sudo', 'modprobe', 'spi-bcm2708'])
+    if modprobe == 0:
+        print 'Kernel module loaded!'
+    else:
+        print 'Error loading kernel module.'
+
+    # Welcome message
     print
     print "To read a tag, scan it with the sensor."
     print "To stop reading, press Ctrl + C."
@@ -37,19 +57,38 @@ initialize()
 RFIDReader = MFRC522.MFRC522()
 
 
-def send_tag_information(tag_uid, dispatcher='console'):
-    """ Send the tag information according to the dispatcher.
+def blink(times):
+    sleep_time = 0.15
+    gpio_led_pin = 17
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(gpio_led_pin, GPIO.OUT)
 
-    TODO: Talk to TS07 to determine the format and how to send the information
-    to them.
-    """
-    if dispatcher == 'console':
+    for x in xrange(times):
+        GPIO.output(gpio_led_pin, True)
+        time.sleep(sleep_time)
+        GPIO.output(gpio_led_pin, False)
+        time.sleep(sleep_time)
+
+
+def send_tag_information(tag_uid):
+    if DISPATCHER == 'console':
         print 'The tag value is: ', tag_uid
-    elif dispatcher == 'web':
+        blink(3)
+    elif DISPATCHER == 'web':
         URL = 'http://sigac.herokuapp.com/tag/'
         response = requests.post(URL, data=json.dumps({'tag': tag_uid}))
+        if response.status_code == 200:
+            blink(1)
+        elif response.status_code >= 400:
+            blink(2)
+        else:
+            blink(3)
     else:
         print 'Action not supported yet.'
+
+
+def convert_uid_to_hex(uid):
+    return ''.join(map(lambda x: str(hex(x))[2:], uid[0:4]))
 
 
 # This loop keeps checking for chips.
@@ -64,7 +103,15 @@ while CONTINUE_READING:
     # Get the UID of the card.
     (status,uid) = RFIDReader.MFRC522_Anticoll()
 
+    # Convert the tag UID to hex and test if that tag has already been
+    # saved.
+    hex_uid = convert_uid_to_hex(uid)
+    if hex_uid in RFID_TAGS:
+        continue
+
+    RFID_TAGS.add(hex_uid)
+
     # If we have the UID, continue.
     if status == RFIDReader.MI_OK:
         # Handle the tag information.
-        send_tag_information(uid[0:4])
+        send_tag_information(hex_uid)
