f9698769bafb12fc9fb974d62f06ee6d264af43d
Diogo Freitas <diogolaco@gmail.com>
2015-11-22 14:39:20 -0200

    cadastro de boletim de ocorrencia

---
+++ web/public/js/pages/registerOR.js
@@ -0,0 +1,34 @@
+registerOR = function() {
+    var _api_insert_OR = function () {
+        var objForm = $('#formInsertOR').serializeObject();
+
+        if (!objForm.officerid) {
+            alert("The field can not be empty");
+            return false;
+        }
+
+        var requestData = JSON.stringify(objForm);
+
+        $.ajax({
+            url: '/api/insert-OR',
+            type: 'POST',
+            async: false,
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            data: requestData
+        }).done(function(data, textStatus, jqXHR) {
+            //$("#labelMsg").html("Equipment included with successfully!");
+            $( '#formInsertOR' ).each(function(){
+                this.reset();
+            });
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+            alert(jqXHR.responseJSON.message);
+        });
+
+        return false;
+    }
+
+    return {
+        api_insert_OR: _api_insert_OR
+    }
+}();
