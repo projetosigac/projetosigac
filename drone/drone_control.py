e62bb15ac0060ae955d56471f2929888c147d06f
Victor Elias <victorgelias@gmail.com>
2015-11-24 06:17:16 -0200

    Implementing code to capture image from drone camera using OpenCV
    Obviously requires OpenCV to be installed on the machine.

--- drone/drone_control.py
+++ drone/drone_control.py
@@ -1,6 +1,7 @@
 import time
 import thread
 import threading
+import cv2
 
 from api import libardrone
 
@@ -8,7 +9,7 @@ class DroneController ( object ):
   def __init__(self):
     self.drone = libardrone.ARDrone()
     self.lock = threading.Lock()
-    self.image = None
+    self.mission_picture = self.cam = None
 
   def __move_drone(self, forward, side):
     if forward > 0:
@@ -39,7 +40,7 @@ class DroneController ( object ):
       time.sleep(5)
 
       self.__move_drone(forward, side)
-      # take picture, setting self.image
+      self.mission_picture = self.take_picture()
       self.__move_drone(-forward, -side)
 
       self.drone.land()
@@ -47,8 +48,38 @@ class DroneController ( object ):
     finally:
       self.lock.release()
 
-  def get_image(self):
-    return self.image
+  def __init_cam(self, debug=False):
+    if self.cam != None:
+      if self.cam.isOpened():
+        return True
+      else:
+        self.cam.release()
+        self.cam = None
+
+    self.cam = cv2.VideoCapture('tcp://192.168.1.1:5555')
+    if not self.cam.isOpened() and debug:
+      self.cam.release()
+      self.cam = cv2.VideoCapture(0)
+    if not self.cam.isOpened():
+      self.cam.release()
+      self.cam = None
+      return False
+    return True
+
+  def take_picture(self, max_retries=5, debug=False):
+    try_count = 0
+    while ++try_count <= max_retries and self.__init_cam(debug):
+      running, frame = self.cam.read()
+      if not running:
+        self.cam.release()
+        continue  # retry
+
+      s, img = cv2.imencode('.jpg', frame)
+      if s: return img.tostring()
+    return None
+
+  def get_mission_picture(self):
+    return self.mission_picture
 
   def start_drone_mission(self, lat, lng):
     thread.start_new_thread(self.__drone_mission_sync, (lat, lng))
