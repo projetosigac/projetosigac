1d955fa598867691b8711ac931ab67546196792b
Victor Elias <victorgelias@gmail.com>
2015-11-24 17:37:49 -0200

    Separating drone camera logic on a separate class
    Cleaner.

---
+++ drone/drone_camera.py
@@ -0,0 +1,82 @@
+import time
+import threading
+import cv2
+
+class DroneCamera ( object ):
+  def __init__(self, debug=False):
+    self.debug = debug
+    self.cam = None
+    self.lock = threading.RLock()
+
+    self.is_running = True
+    self.grabber_thread = threading.Thread(target=self.__grab_frames)
+    self.grabber_thread.start()
+
+  def shutdown(self):
+    if self.cam != None:
+      self.cam.release()
+      self.cam = None
+
+    self.is_running = False
+    self.grabber_thread.join()
+
+    cv2.destroyAllWindows()
+
+  def __grab_frames(self, fps=25):
+    start_time = time.time()
+    grab_count = 0
+    while self.is_running:
+      time.sleep(1)
+      target_grabs = (time.time() - start_time) * fps
+      if not self.cam or not self.cam.isOpened():
+        grab_count = target_grabs
+        continue
+
+      self.lock.acquire()
+      try:
+        while grab_count < target_grabs:
+          grab_count += 1
+          self.cam.grab()
+      finally:
+        self.lock.release()
+
+  def __init_cam(self):
+    self.lock.acquire()
+    try:
+      if self.cam != None:
+        if self.cam.isOpened():
+          return True
+        else:
+          self.cam.release()
+          self.cam = None
+
+      self.cam = cv2.VideoCapture('tcp://192.168.1.1:5555')
+      if not self.cam.isOpened() and self.debug:
+        self.cam.release()
+        self.cam = cv2.VideoCapture(0)  # from webcam
+      if not self.cam.isOpened():
+        self.cam.release()
+        self.cam = None
+        return False
+      return True
+    finally:
+      self.lock.release()
+
+  def snapshot(self, max_retries=5):
+    self.lock.acquire()
+    try:
+      try_count = 0
+      while try_count < max_retries and self.__init_cam():
+        try_count += 1
+        running, frame = self.cam.read()
+        if not running:
+          self.cam.release()
+          continue  # retry
+
+        s, img = cv2.imencode('.jpg', frame)
+        if s:
+          if self.debug: print 'Picture taken from camera'
+          return img.tostring()
+      return None
+    finally:
+      self.lock.release()
--- drone/drone_control.py
+++ drone/drone_control.py
@@ -4,19 +4,15 @@ import threading
 import cv2
 
 from api import libardrone
+from drone_camera import DroneCamera
 
 class DroneController ( object ):
   def __init__(self, debug=False):
     self.debug = debug
     self.drone = libardrone.ARDrone()
     self.drone_lock = threading.Lock()
-
-    self.mission_picture = self.cam = None
-    self.cam_lock = threading.RLock()
-
-    self.is_running = True
-    self.grabber_thread = threading.Thread(target=self.__grab_frames)
-    self.grabber_thread.start()
+    self.mission_picture = None
+    self.cam = DroneCamera(debug)
 
   def shutdown(self):
     if self.drone != None:
@@ -25,15 +21,7 @@ class DroneController ( object ):
       self.drone.halt()
       self.drone = None
       self.drone_lock.release()
-
-    if self.cam != None:
-      self.cam.release()
-      self.cam = None
-
-    self.is_running = False
-    self.grabber_thread.join()
-
-    cv2.destroyAllWindows()
+    self.cam.shutdown()
 
   def __move_drone(self, forward, side):
     if forward != 0:
@@ -82,64 +70,8 @@ class DroneController ( object ):
     finally:
       self.drone_lock.release()
 
-  def __init_cam(self):
-    self.cam_lock.acquire()
-    try:
-      if self.cam != None:
-        if self.cam.isOpened():
-          return True
-        else:
-          self.cam.release()
-          self.cam = None
-
-      self.cam = cv2.VideoCapture('tcp://192.168.1.1:5555')
-      if not self.cam.isOpened() and self.debug:
-        self.cam.release()
-        self.cam = cv2.VideoCapture(0)  # from webcam
-      if not self.cam.isOpened():
-        self.cam.release()
-        self.cam = None
-        return False
-      return True
-    finally:
-      self.cam_lock.release()
-
-  def __grab_frames(self, fps=25):
-    start_time = time.time()
-    grab_count = 0
-    while self.is_running:
-      time.sleep(1)
-      target_grabs = (time.time() - start_time) * fps
-      if not self.cam or not self.cam.isOpened():
-        grab_count = target_grabs
-        continue
-
-      self.cam_lock.acquire()
-      try:
-        while grab_count < target_grabs:
-          grab_count += 1
-          self.cam.grab()
-      finally:
-        self.cam_lock.release()
-
   def take_picture(self, max_retries=5):
-    self.cam_lock.acquire()
-    try:
-      try_count = 0
-      while try_count <= max_retries and self.__init_cam():
-        try_count += 1
-        running, frame = self.cam.read()
-        if not running:
-          self.cam.release()
-          continue  # retry
-
-        s, img = cv2.imencode('.jpg', frame)
-        if s:
-          if self.debug: print 'Picture taken from camera'
-          return img.tostring()
-      return None
-    finally:
-      self.cam_lock.release()
+    return self.cam.snapshot()
 
   def get_mission_picture(self):
     return self.mission_picture
