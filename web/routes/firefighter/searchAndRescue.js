c6133ba8fa15d0b5855e6493299d3dcb834dcb36
samuelmtimbo <samuelmtimbo@gmail.com>
2015-11-27 00:56:30 -0200

    Firefighter Search and Rescue portal page implemented

--- web/routes/firefighter/searchAndRescue.js
+++ web/routes/firefighter/searchAndRescue.js
@@ -24,9 +24,60 @@ module.exports = function (app) {
     };
 
     app.get('/dashboard/firefighter/sar', /*util.autenticarSessao,*/ function(req, res) {
-        var options = Object.create(commonOptions);
+        req.getConnection(function(err, connection) {
+            connection.query('SELECT * FROM vitimas', function(err, victms) {
+                if (err) {
+                    console.log("Error Selecting : %s ", err);
+                    res.status(500);
+                    res.send();
+                    return;
+                }
 
-        options.contentTemplate = 'victims_view';
-        return res.render('firefighter/searchandrescue/index', options);
+                console.log(victms);
+
+                var options = Object.create(commonOptions);
+                options.victms = victms;
+                res.render('firefighter/searchandrescue/index', options);
+            });
+        });
+    });
+
+    app.post('/dashboard/firefighter/sar', function(req, res) {
+        var data = req.body;
+
+        req.getConnection(function(err, connection) {
+            var token = data.token;
+            var nome = data.name;
+            var sexo = data.gender;
+
+            var values = '\"' + token + '\", \"' + nome + '\", \"' + sexo + '\"';
+            var query = 'INSERT INTO vitimas (token, nome, sexo) VALUES (' + values + ')';
+
+            connection.query(query, function(err) {
+                if (err) {
+                    console.log("Error Selecting : %s ", err);
+                    res.status(500);
+                    res.send('ERROR');
+                    return;
+                }
+
+
+
+                connection.query('SELECT * FROM vitimas', function(err, victms) {
+                    if (err) {
+                        console.log("Error Selecting : %s ", err);
+                        res.status(500);
+                        res.send();
+                        return;
+                    }
+
+                    // console.log(victms);
+
+                    var options = Object.create(commonOptions);
+                    options.victms = victms;
+                    res.render('firefighter/searchandrescue/index', options);
+                });
+            });
+        });
     });
 }
